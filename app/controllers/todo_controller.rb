be8f1c4d20f10322930b450f14dab9910896231d
David <david.molina.calatayud@gmail.com>
2018-07-12 12:52:38 +0000

    Testing commit

---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,14 @@
+class TodoController<ApplicationController>
+    def index 
+    end
+    def create 
+        t= Todo.new    
+        t.description= params['description']
+        t.pomodoro_estimate= params['pomodoro_estimate']
+        t.save 
+        redirect_to "/todo/show/#{t.id}"
+    end
+    def edit 
+        @todo=Todo.find_by_id(params[:id])
+    end
+end
